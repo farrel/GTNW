647290814107c426e551e18b3fdc848b7083d50b
Farrel Lifson <farrel.lifson@aimred.com>
2014-11-24 01:07:20 +0200

    Messing around with status bar

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![feature(globs)]
-
 extern crate ncurses;
+extern crate time;
+
 use ncurses::*;
+
+mod status_bar;
 
 fn initialise_ncurses() {
     initscr();
@@ -11,6 +14,7 @@ fn initialise_ncurses() {
 fn main() {
     initialise_ncurses();
 
+
     /* Get the screen bounds. */
     let mut max_x = 0;
     let mut max_y = 0;
@@ -18,15 +22,17 @@ fn main() {
     getmaxyx(stdscr, &mut max_y, &mut max_x);
 
     let main_window  = newwin(max_y - 2,max_x,0,0);
-    let status_bar   = newwin(1,max_x,max_y - 2,0);
+    let status_bar_window   = newwin(1,max_x,max_y - 2,0);
     let input_window = newwin(1,max_x,max_y - 1,0);
+
+    let status_bar = status_bar::StatusBar { window: status_bar_window, defcon: 5 };
+    status_bar.initialise();
+
     
     scrollok(main_window, true);
 
-    waddstr(status_bar, "STATUS");
     waddstr(input_window,">>");
 
-    wrefresh(status_bar);
     wrefresh(input_window);
 
     wmove(main_window, max_y - 3,0);
@@ -34,11 +40,9 @@ fn main() {
 
     for x in range(0i32, 200i32) {
         wprintw(main_window, format!("{}\n",x).as_slice());
-        wmove(status_bar,0,0);
-        wprintw(status_bar, format!("{}\n",x).as_slice());
 
-        wrefresh(status_bar);
         wrefresh(main_window);
+        status_bar.draw();
         std::io::timer::sleep(std::time::duration::Duration::seconds(1));
     }
     endwin();
